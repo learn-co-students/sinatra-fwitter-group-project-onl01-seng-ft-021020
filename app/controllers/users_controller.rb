05c01792f51a5e7ff926b735b8141771ded6b56c
Steven Perfors <stevecanbuild@gmail.com>
2020-04-28 21:52:20 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
   end
   
   post '/signup' do 
-    
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save 
+      redirect "/login"
+    else 
+      redirect "/failure"
+    end
   end
   
   get '/login' do 
@@ -13,7 +18,14 @@ class UsersController < ApplicationController
   end
   
   post '/login' do 
+    user = User.find_by(:username => params[:username])
     
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect "/succes"
+    else 
+      redirect "/failure"
+    end
   end
   
   
