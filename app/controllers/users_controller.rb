4f8b90d3a9b5484e48a65ef319fc4f51a0c35c7d
Steven Perfors <stevecanbuild@gmail.com>
2020-04-28 21:44:10 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
 
-
+  get '/signup' do 
+    
+  end
+  
+  post '/signup' do 
+    
+  end
+  
+  get '/login' do 
+    
+  end
+  
+  post '/login' do 
+    
+  end
+  
+  
+  get '/logout' do 
+    
+  end
+  
 end
